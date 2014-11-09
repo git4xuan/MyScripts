6e422e20c6b2578da3d01899539405b95d0026cc
root <root@v4host01.pengfeng.xyz>
2014-11-08 23:42:05 -0500

    change : if the files or folder exists . The files will not created

--- SSH/addpubkey.sh
+++ SSH/addpubkey.sh
@@ -2,14 +2,17 @@
 
 #The VPS was first created and I have to configture it.
 #Here is a Script to add my personal public Key.
+#
+if   [ ! -d ~/.ssh/ ] ; then
+	mkdir ~/.ssh
+fi
 
-mkdir /root/.ssh/
 mv /etc/ssh/sshd_config{,.bak}
 rm -f /etc/ssh/sshd_config
 cat ./files/sshd_config_sample >>  /etc/ssh/sshd_config
-cat ./files/id_rsa_2048onekey.pub >> /root/.ssh/authorized_keys
-chmod 600   /root/.ssh/authorized_keys
-chmod 700  /root/.ssh/
+cat ./files/id_rsa_2048onekey.pub >>  ~/.ssh/authorized_keys
+chmod 600   ~/.ssh/authorized_keys
+chmod 700   ~/.ssh/
 /etc/init.d/sshd restart
 
 
